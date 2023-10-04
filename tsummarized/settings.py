63b25a125e578c1652ee37f72ec8ac26627aec66
muhammadadildeveloper999 <121722059+muhammadadildeveloper999@users.noreply.github.com>
2023-10-05 00:24:16 +0500

    Update settings.py

--- tsummarized/settings.py
+++ tsummarized/settings.py
@@ -27,7 +27,7 @@ os.environ["OPENAI_API_KEY"] = "sk-JDMyJmFMml6HJx0LYxo7T3BlbkFJvGsv4JrxpKq08BYQU
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
